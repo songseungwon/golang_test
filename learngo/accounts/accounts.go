49f6879ded66e0eac9c2c0b9e13b5c5d68b31a3d
swsong <sw930601@gmail.com>
2020-06-07 14:10:37 +0900

    "banking system"

--- learngo/accounts/accounts.go
+++ learngo/accounts/accounts.go
@@ -1,7 +1,36 @@
-package banking
+package accounts
+
+import "errors"
 
 //Account struct
 type Account struct {
-	owner   string
-	balance int
+	owner   string //예금주
+	balance int    //잔고
+}
+
+//NewAccount creates Account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+//Deposit + amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+//Balance of your account
+func (a *Account) Balance() int {
+	return a.balance
+}
+
+var errNoMoney = errors.New("Can't withdraw. You are poor")
+
+//Withdraw on your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
---
+++ learngo/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/learngo/accounts"
+)
+
+func main() {
+	account := accounts.NewAccount("nico")
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
+}
